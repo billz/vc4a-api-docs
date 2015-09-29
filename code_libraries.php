50f04e5163017288424355143c3dd6e26024928f
Bill Zimmerman <billzimmerman@gmail.com>
2015-09-29 11:56:36 +0200

    Cleanup old content, updated code library links

--- code_libraries.php
+++ code_libraries.php
@@ -13,27 +13,17 @@
 <div id="content">
 
 <h2>VC4A API Code Libraries</h2>
-<p>We maintain VC4A API clients for Java, PHP and Python on our <a href="https://github.com/vc4a">public GitHub page</a>.
-You are free to download or fork these repos as starting points for your own projects. We also welcome contributions to our code library and encourage you to let us know if you've developed a module or project.
+<p>We currently maintain a minimal VC4A API clients for JavaScript. You are free to download or fork this repo as starting points for your own projects. We also welcome contributions to our code library and encourage you to let us know if you've developed a module or project.
 While we cannot support third-party contributions, we're glad to include them in this list!</p>
 
 <p>If you have a question about these libraries or wish to submit one of your own, please contact us via the VC4A <a href="api_discussion_group.php">API discussion group</a>.</p>
 
-<h3>Java</h3>
+<h3>JavaScript</h3>
 <ul>
-<li><a href="https://github.com/vc4a/vc4a-java-example" target="_blank">https://github.com/vc4a/vc4a-java-example</a> <strong>(official Java client)</strong></li>
+<li><a href="https://github.com/billz/vc4a-api-docs/tree/master/sample-code/javascript" target="_blank">https://github.com/billz/vc4a-api-docs/tree/master/sample-code/javascript</a> <strong>(official JavaScript client)</strong></li>
 </ul>
-<h3>PHP</h3>
 <ul>
-<li><a href="https://github.com/vc4a/vc4a-php-example" target="_blank">https://github.com/vc4a/vc4a-php-example</a> <strong>(official PHP client)</strong></li>
-</ul>
-<h3>Python</h3>
-<ul>
-<li><a href="https://github.com/vc4a/vc4a-python-example" target="_blank">https://github.com/vc4a/vc4a-python-example</a> <strong>(official Python client)</strong></li>
-</ul>
-<h3>javascript - Node.js</h3>
-<ul>
-<li>TBD</li>
+<li><a href="https://jsfiddle.net/billz/w4komwaf/" target="_blank">https://jsfiddle.net/billz/w4komwaf/</a> <strong>(JSFiddle example)</strong></li>
 </ul>
 </div>
 
--- static/js_examples/api_key_include.js
+++
@@ -1,5 +0,0 @@
-{
-  "api_key":"***REPLACE ME WITH YOUR API KEY***", 
-  "api_login":"***REPLACE ME WITH YOUR LOGIN***", 
-  "api_url":"api.VC4A.com"
-}
